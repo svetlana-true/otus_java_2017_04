e3154320d7930acd58feeac112c36837db9b4b49
svetlana-true <svetlana.true.zr@gmail.com>
2017-04-05 20:05:26 +0300

    First project

---
+++ src/main/java/otus/First.java
@@ -0,0 +1,21 @@
+package otus;
+
+import java.util.logging.Logger;
+
+
+/**
+ * Created by svetlana on 4/5/17.
+ */
+
+public class First {
+
+    public static void main(String args[]) throws Exception {
+        Logger log = Logger.getLogger(First.class.getName());
+
+        String str = "Hello there";
+        log.info("Starting project: " + str);
+
+
+    }
+
+}
