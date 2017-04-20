25b8d3e17bc8b8bf4dcf36a7d291b527323c84ee
svetlana-true <svetlana.true.zr@gmail.com>
2017-04-20 18:32:42 +0300

    Add homework3.

--- homework3/src/main/java/otus/TestArrayList.java
+++ homework3/src/main/java/otus/TestArrayList.java
@@ -14,32 +14,44 @@ public class TestArrayList
 {
     public static void main(String[] args) throws Exception
     {
-        String[] first = new String[] { "one", "two", "three" };
-        ArrayList<String> arrayList = new ArrayList<>(Arrays.asList(first));
-        System.out.println("ArrayList: " + Arrays.toString(arrayList.toArray()));
-
-        String[] second = new String[] { "four", "five", "six" };
-        OwnArrayList<String> ownArrayList = new OwnArrayList(second);
+        String[] arrayForOwnList = new String[] { "one", "two", "three" };;
+        OwnArrayList<String> ownArrayList = new OwnArrayList(arrayForOwnList);
         System.out.println("Own ArrayList: " + Arrays.toString(ownArrayList.toArray()));
 
-        addAll(arrayList, second);
-        System.out.println("ArrayList with add: " + Arrays.toString(arrayList.toArray()));
+        String[] arrayForAdd = new String[] { "four", "five", "six" };
+        addAll(ownArrayList, arrayForAdd);
+        System.out.println("Testing of \"addAll\": add " +  Arrays.toString(arrayForAdd) + " in own ArrayList: " + Arrays.toString(ownArrayList.toArray()));
 
-        addAll(ownArrayList, first);
-        System.out.println("Own ArrayList with add: " + Arrays.toString(ownArrayList.toArray()));
+        String[] arrayForList = new String[] {"seven", "eight", "nine" };
+        ArrayList<String> arrayList = new ArrayList<>(Arrays.asList(arrayForList));
+        System.out.println("ArrayList: " + Arrays.toString(arrayList.toArray()));
 
-
-        String[] third = new String[] { "seven", "eight", "nine" };
-        ArrayList<String> arrayList2 = new ArrayList<>(Arrays.asList(third));
-        System.out.println("ArrayList2: " + Arrays.toString(arrayList2.toArray()));
-
-        copy(ownArrayList, arrayList2);
-        System.out.println("Copy ArrayList to own ArrayList: " + Arrays.toString(ownArrayList.toArray()));
-
-        sort(arrayList);
-        System.out.println("Sorted ArrayList: " + Arrays.toString(arrayList.toArray()));
+        copy(ownArrayList, arrayList);
+        System.out.println("Testing of \"copy\": copy ArrayList to own ArrayList: " + Arrays.toString(ownArrayList.toArray()));
 
         sort(ownArrayList);
-        System.out.println("Sorted own ArrayList: " + Arrays.toString(ownArrayList.toArray()));
+        System.out.println("Testing of \"sort\": sort for own ArrayList: " + Arrays.toString(ownArrayList.toArray()));
+
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+
+        Integer[] intArrayForOwnList = new Integer[] { 1, 2, 3 };
+        OwnArrayList<Integer> ownArrayListInt = new OwnArrayList(intArrayForOwnList);
+        System.out.println("Own ArrayList: " + Arrays.toString(ownArrayListInt.toArray()));
+
+        Integer[] intArrayForAdd = new Integer[] { 4, 5, 6 };
+        addAll(ownArrayListInt, intArrayForAdd);
+        System.out.println("Testing of \"addAll\": add " + Arrays.toString(intArrayForAdd) + " in own ArrayList: " + Arrays.toString(ownArrayListInt.toArray()));
+
+        Integer[] intArrayForList = new Integer[] { 7, 8, 9 };
+        ArrayList<Integer> arrayListInt = new ArrayList<>(Arrays.asList(intArrayForList));
+        System.out.println("ArrayList: " + Arrays.toString(arrayListInt.toArray()));
+
+        copy(ownArrayListInt, arrayListInt);
+        System.out.println("Testing of \"copy\": copy ArrayList to own ArrayList: " + Arrays.toString(ownArrayListInt.toArray()));
+
+        sort(ownArrayListInt);
+        System.out.println("Testing of \"sort\": sort for own ArrayList: " + Arrays.toString(ownArrayListInt.toArray()));
+
+
     }
 }
