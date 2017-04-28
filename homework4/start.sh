968b2b181f70b3af084f3419fc1caba0090ca35f
svetlana-true <svetlana.true.zr@gmail.com>
2017-04-28 09:21:32 +0300

    Add homework 4.

--- homework4/start.sh
+++ homework4/start.sh
@@ -1,9 +1,11 @@
 #Script for testing the different GCs
 
-java -jar target/homework4-1.0-SNAPSHOT.jar 
+java -jar target/homework4-1.0-SNAPSHOT.jar
 
-#java -XX:+UseSerialGC -jar target/homework4-1.0-SNAPSHOT.jar 
+#java -Xmx512m -Xms512m -XX:+UseSerialGC -jar target/homework4-1.0-SNAPSHOT.jar
 
-#java -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -jar target/homework4-1.0-SNAPSHOT.jar 
+#java -Xmx512m -Xms512m -XX:+UseParallelGC -jar target/homework4-1.0-SNAPSHOT.jar
 
-#java -XX:+UseG1GC -jar target/homework4-1.0-SNAPSHOT.jar 
+#java -Xmx512m -Xms512m -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -jar target/homework4-1.0-SNAPSHOT.jar
+
+#java -Xmx512m -Xms512m -XX:+UseG1GC -jar target/homework4-1.0-SNAPSHOT.jar
