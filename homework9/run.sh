4e098fdf193a83aaca3e4581d4f5cae4833d1c04
svetlana-true <svetlana.true.zr@gmail.com>
2017-06-08 22:05:06 +0300

    Add homework 9.

---
+++ homework9/run.sh
@@ -0,0 +1,2 @@
+mvn clean package;
+java -jar target/homework9-1.0-SNAPSHOT.jar
---
+++ homework9/src/main/java/otus/ORM.java
@@ -0,0 +1,12 @@
+package otus;
+
+/**
+ * Created by svetlana on 6/8/17.
+ */
+public class ORM {
+    public static void main(String args[]) throws Exception {
+        System.out.println("Here is my ORM. ");
+
+
+    }
+}
